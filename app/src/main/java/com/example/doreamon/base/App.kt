698fd7c6b3df703290f64ce8fe716c29b9667b1f
zhanhua <913447093@qq.com>
2021-11-08 14:25:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/doreamon/base/App.kt
@@ -0,0 +1,8 @@
+package com.example.doreamon.base
+
+/**
+ * @author wzh
+ * @date 2021/11/8
+ */
+class App {
+}
---
+++ app/src/main/java/com/example/doreamon/utils/MD5Util.java
@@ -0,0 +1,8 @@
+package com.example.doreamon.utils;
+
+/**
+ * @author wzh
+ * @date 2021/11/8
+ */
+class Md5Util {
+}
